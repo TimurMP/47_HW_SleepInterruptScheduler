ad747d5767b4f71cea1c1e61d4c5ed08b578df3c
TimurMP <titimurmamyshev@gmail.commur>
2022-08-28 17:47:14 +0300

    Baseline

--- src/telran/printer/controller/PrinterAppl.java
+++ src/telran/printer/controller/PrinterAppl.java
@@ -1,2 +1,11 @@
-package telran.printer.controller;public class PrinterAppl {
+package telran.printer.controller;
+
+import telran.printer.tasks.Printer;
+
+public class PrinterAppl {
+
+    public static void main(String[] args) {
+        Printer printer = new Printer(1, 10, 3);
+        printer.run();
+    }
 }
--- src/telran/printer/tasks/Printer.java
+++ src/telran/printer/tasks/Printer.java
@@ -1,2 +1,31 @@
-package telran.printer.tasks;public class Printer {
+package telran.printer.tasks;
+
+public class Printer implements Runnable{
+    int numberToPrint;
+    int numberAmount;
+    int portion;
+
+    public Printer(int numberToPrint, int numberAmount, int portion) {
+        this.numberToPrint = numberToPrint;
+        this.numberAmount = numberAmount;
+        this.portion = portion;
+    }
+
+    @Override
+    public void run() {
+        int count = numberAmount/portion;
+
+        for (int i = 0; i < count; i++) {
+            for (int j = 0; j < portion; j++) {
+                System.out.print(numberToPrint);
+
+            }
+            System.out.println();
+
+        }
+
+
+
+
+    }
 }
