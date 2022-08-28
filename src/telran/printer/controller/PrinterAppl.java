a719491d0ab7e1f6faf1ec7e49a15eaadd28811a
TimurMP <titimurmamyshev@gmail.commur>
2022-08-28 18:13:01 +0300

    Baseline

--- src/telran/printer/controller/PrinterAppl.java
+++ src/telran/printer/controller/PrinterAppl.java
@@ -5,10 +5,35 @@ import telran.printer.tasks.Printer;
 public class PrinterAppl {
 
     public static void main(String[] args) {
-        Printer printer = new Printer(1, 10, 3);
-        Thread thread = new Thread(printer);
+        Printer[] printers = new Printer[2];
+        Thread[] threads = new  Thread[printers.length];
 
-        thread.start();
+        for (int i = 0; i < threads.length; i++) {
+            printers[i] = new Printer(i+1, 100,10);
+            threads[i] = new Thread(printers[i]);
+            threads[i].start();
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].interrupt();
+
+        }
+
+
+//        Printer printer = new Printer(1, 100, 10);
+//        Thread thread = new Thread(printer);
+//        Printer printer2 = new Printer(2, 100, 10);
+//        Thread thread2 = new Thread(printer2);
+//
+//        thread.start();
+//        thread2.start();
+//
+//        thread.interrupt();
+//        thread2.interrupt();
+//        thread.interrupt();
+//        thread2.interrupt();
+
+
 
     }
 }
--- src/telran/printer/tasks/Printer.java
+++ src/telran/printer/tasks/Printer.java
@@ -14,21 +14,25 @@ public class Printer implements Runnable{
     @Override
     public void run() {
 
-        try {
-            Thread.sleep(Long.MAX_VALUE);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+
 
 
         int count = numberAmount/portion;
 
         for (int i = 0; i < count; i++) {
-            for (int j = 0; j < portion; j++) {
-                System.out.print(numberToPrint);
+            try {
+                Thread.sleep(Long.MAX_VALUE);
+            } catch (InterruptedException e) {
+                for (int j = 0; j < portion; j++) {
+                    System.out.print(numberToPrint);
+
+                }
+                System.out.println();
 
             }
-            System.out.println();
+
+
+
 
         }
 
